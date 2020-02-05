8e29e4f15b4214fb83bdea622614b04b4aa064cb
Bryan Boconzaca <xxbryanxx31@gmail.com>
2020-02-05 18:31:48 -0500

    Added question component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-// import components
+import Question from "./Question"
 
 class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        Trivia!
-        
+      <div>
+        <Question
+          question="What day is ?"
+          answer1="day"
+          answer2="tomorrow" 
+          correctAns="day" />
+        <Question
+          question="What day is Christmas?"
+          answer1="day"
+          answer2="tmmr" />
+        <Question
+          question="What day is Christmas?"
+          answer1="day"
+          answer2="tmmr" />
+        <Question
+          question="What day is Christmas?"
+          answer1="day"
+          answer2="tmmr" />
       </div>
+
     );
   }
 }
 
 export default App;
+
+
+{/* <div className="app">
+Trivia!
+hi
+
+</div> */}
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-// import components
+
 
 
 class Question extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      rightAnswer: false
+    }
+    this.onClick = this.onClick.bind(this)
+  }
+  onClick(clickAnswer, correctAns) {
+    console.log(clickAnswer, correctAns)
+    if (correctAns == clickAnswer) {
+      this.setState({rightAnswer: true})
+    }
+    else (
+      this.setState({rightAnswer: false})
+    )
+  }
+  showRightAns() {
+    if (this.state.rightAnswer) {
+      return <text>Correct</text>
+    }
+    else {
+      return <text>INCORRECT</text>
+  }
+  }
   render() {
     return (
       <div>
-        Add your QuestionText, Reset Button and AnswerButtons here.  
-      </div>
+        <div>
+          {this.props.question}
+          <button onClick={() => {
+            this.onClick(this.props.answer1, this.props.correctAns)
+          }}>{this.props.answer1}</button>
+          <button onClick={() => {
+            this.onClick(this.props.answer2, this.props.correctAns)
+          }}>{this.props.answer2}</button>
+
+        </div>{
+        this.showRightAns()
+        }
+        </div>
     );
   }
 }
